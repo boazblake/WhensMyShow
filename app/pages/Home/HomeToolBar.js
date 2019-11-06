6d90f38baef3cba9001fbf8d14be02394b327009
bblake <bblake@goempyrean.com>
2019-11-06 15:42:13 -0600

    fixing dropdown

--- app/pages/Home/HomeToolBar.js
+++ app/pages/Home/HomeToolBar.js
@@ -3,24 +3,30 @@ import { ListSelector } from "../../components/Elements.js"
 
 const FilterLists = () => {
   const selectList = (mdl, list) => mdl.state.currentList(list)
+  let show = true
   return {
     view: ({ attrs: { mdl } }) =>
       m(".dropdown", [
-        m("a.btn btn-link dropdown-toggle", { tabindex: "0" }, [
-          mdl.state.currentList(),
-          m("i.icon icon-caret")
-        ]),
         m(
-          "ul.menu",
-          mdl.user.lists().map((list, idx) =>
-            m(ListSelector, {
-              list,
-              action: () => selectList(mdl, list),
-              key: idx,
-              mdl
-            })
+          "a.btn btn-link dropdown-toggle",
+          { onclick: () => (show = true), tabindex: "0" },
+          [mdl.state.currentList(), m("i.icon icon-caret")]
+        ),
+        show &&
+          m(
+            "ul.menu",
+            mdl.user.lists().map((list, idx) =>
+              m(ListSelector, {
+                list,
+                action: () => {
+                  selectList(mdl, list)
+                  show = false
+                },
+                key: idx,
+                mdl
+              })
+            )
           )
-        )
       ])
   }
 }
